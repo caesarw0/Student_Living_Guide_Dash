bddc15b943919d310ba9feba536ab099f0c23e8d
Caesar Wong <caesar.wongw@gmail.com>
2023-03-19 16:21:55 -0700

    set default page table page description page & default bar plot

--- src/app.py
+++ src/app.py
@@ -96,11 +96,12 @@
 
 # V2
 import dash
-from dash import html
-from dash import dcc
+from dash import html, dcc, dash_table
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SANDSTONE])
 
@@ -108,6 +109,8 @@ server = app.server
 
 # get data
 df = pd.read_csv('../data/processed_data.csv')
+# drop unused column
+df = df.drop(['Rank'], axis=1)
 
 # =========================
 # UI
@@ -117,16 +120,6 @@ navbar = dbc.NavbarSimple(
         dbc.NavItem(dbc.NavLink("Overview", href="/")),
         dbc.NavItem(dbc.NavLink("Table", href="/table")),
         dbc.NavItem(dbc.NavLink("Description", href="/description")),
-        # dbc.DropdownMenu(
-        #     children=[
-        #         dbc.DropdownMenuItem("More pages", header=True),
-        #         dbc.DropdownMenuItem("Page 2", href="#"),
-        #         dbc.DropdownMenuItem("Page 3", href="#"),
-        #     ],
-        #     nav=True,
-        #     in_navbar=True,
-        #     label="More",
-        # ),
     ],
     brand="Cost Viewer",
     brand_href="#",
@@ -134,100 +127,138 @@ navbar = dbc.NavbarSimple(
     dark=True,
 )
 
+overview_layout = html.Div([html.H1("Overview"),
+                            dbc.Row([dcc.Graph(id='bar')
+
+                                     ])
+
+                            ], id='overview-content')
+table_layout = html.Div(
+    [html.H2("Filtered Data Table"),
+     dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns],
+                          id='datatable',
+                          page_action='native',
+                          page_current=0,
+                          page_size=15,
+                          style_table={'overflowX': 'auto'})
+
+     ], id='table-content')
+description_layout = html.Div(html.H1("Description"), id='description-content')
+
 app.layout = html.Div([
+    dcc.Location(id="url"),
     html.Nav(navbar, style={"marginBottom": "5px"}),
     html.Div([
         dbc.Row(
             [
-            dbc.Col(
+                dbc.Col(
                     html.Div([
-                            html.Label('Select a continent', style={"marginBottom": "5px"}),
-                            dcc.Dropdown(
-                                    id='continent-dropdown',
-                                    options=[
-                                        {'label': 'Africa', 'value': 'Africa'},
-                                        {'label': 'Asia', 'value': 'Asia'},
-                                        {'label': 'Europe', 'value': 'Europe'},
-                                        {'label': 'North America', 'value': 'North America'},
-                                        {'label': 'South America', 'value': 'South America'},
-                                        {'label': 'Oceania', 'value': 'Oceania'}
-                                    ],
-                                    value=['Africa', 'Asia', 'Europe', 'North America', 'South America', 'Oceania'],
-                                    multi=True,
-                                    placeholder='Select a continent',
-                                    clearable=False
-                                )
-            ])
-                    ),
+                        html.Label('Select a continent', style={
+                                   "marginBottom": "5px"}),
+                        dcc.Dropdown(
+                            id='continent-dropdown',
+                            options=[
+                                {'label': 'Africa', 'value': 'Africa'},
+                                {'label': 'Asia', 'value': 'Asia'},
+                                {'label': 'Europe', 'value': 'Europe'},
+                                {'label': 'North America',
+                                 'value': 'North America'},
+                                {'label': 'South America',
+                                 'value': 'South America'},
+                                {'label': 'Oceania', 'value': 'Oceania'}
+                            ],
+                            value=['Africa', 'Asia', 'Europe',
+                                   'North America', 'South America', 'Oceania'],
+                            multi=True,
+                            placeholder='Select a continent',
+                            clearable=False
+                        )
+                    ])
+                ),
                 dbc.Col(html.Div([
-                                html.Label('Select an interest of index', style={"marginBottom": "5px"}),
-                                dcc.Dropdown(
-                                        id='index-dropdown',
-                                        options=[
-                                            {'label': 'Cost of Living Index', 'value': 'Cost of Living Index'},
-                                            {'label': 'Rent Index', 'value': 'Rent Index'},
-                                            {'label': 'Cost of Living Plus Rent Index', 'value': 'Cost of Living Plus Rent Index'},
-                                            {'label': 'Groceries Index', 'value': 'Groceries Index'},
-                                            {'label': 'Restaurant Price Index', 'value': 'Restaurant Price Index'},
-                                            {'label': 'Local Purchasing Power Index', 'value': 'Local Purchasing Power Index'}
-                                        ],
-                                        value='Cost of Living Index',
-                                        clearable=False
-                                    )])
-                        ),
+                    html.Label('Select an interest of index',
+                               style={"marginBottom": "5px"}),
+                    dcc.Dropdown(
+                        id='index-dropdown',
+                        options=[
+                            {'label': 'Cost of Living Index',
+                             'value': 'Cost of Living Index'},
+                            {'label': 'Rent Index',
+                             'value': 'Rent Index'},
+                            {'label': 'Cost of Living Plus Rent Index',
+                             'value': 'Cost of Living Plus Rent Index'},
+                            {'label': 'Groceries Index',
+                             'value': 'Groceries Index'},
+                            {'label': 'Restaurant Price Index',
+                             'value': 'Restaurant Price Index'},
+                            {'label': 'Local Purchasing Power Index',
+                             'value': 'Local Purchasing Power Index'}
+                        ],
+                        value='Cost of Living Index',
+                        clearable=False
+                    )])
+                ),
                 dbc.Col(html.Div([
-                                html.Label(id='index-range-label', children='Select a range',style={"marginBottom": "5px"}),
-                                dcc.RangeSlider(
-                                        id='index-slider',
-                                        min=0,
-                                        max=160,
-                                        step=20,
-                                        value=[0, 160],
-                                        marks={
-                                            0: {'label': '0', 'style': {'color': '#77b0b1'}},
-                                            20: {'label': '20'},
-                                            40: {'label': '40'},
-                                            60: {'label': '60'},
-                                            80: {'label': '80'},
-                                            100: {'label': '100 (NYC)', 'style': {'color': '#1950b5'}},
-                                            120: {'label': '120'},
-                                            140: {'label': '140'},
-                                            160: {'label': '160', 'style': {'color': '#f50'}}},
-                                        allowCross=False,
-                                        tooltip={"placement": "bottom", "always_visible": True}
-                                    )
-                                ])
-                        ),
-            ]
-        , style={"padding": "10px"}),
+                    html.Label(
+                        id='index-range-label', children='Select a range', style={"marginBottom": "5px"}),
+                    dcc.RangeSlider(
+                        id='index-slider',
+                        min=0,
+                        max=160,
+                        step=20,
+                        value=[0, 160],
+                        marks={
+                            0: {'label': '0', 'style': {'color': '#77b0b1'}},
+                            20: {'label': '20'},
+                            40: {'label': '40'},
+                            60: {'label': '60'},
+                            80: {'label': '80'},
+                            100: {'label': '100 (NYC)', 'style': {'color': '#1950b5'}},
+                            120: {'label': '120'},
+                            140: {'label': '140'},
+                            160: {'label': '160', 'style': {'color': '#f50'}}},
+                        allowCross=False,
+                        tooltip={"placement": "bottom",
+                                 "always_visible": True}
+                    )
+                ])
+                ),
+            ], style={"padding": "10px"}),
         dbc.Row([
             dbc.Alert(
-                    "Index slider value must be a range instead of a single value",
-                    id="alert-fade",
-                    dismissable=True,
-                    is_open=False,
-                    color='warning'
-                ),
+                "Index slider value must be a range instead of a single value",
+                id="alert-fade",
+                dismissable=True,
+                is_open=False,
+                color='warning'
+            ),
             dbc.Alert(
-                    "Must select at least 1 continent (set to default selection with all continents)",
-                    id="alert-fade2",
-                    dismissable=True,
-                    is_open=False,
-                    color='warning'
-                )]
-        , style={"padding": "10px"})
+                "Must select at least 1 continent (set to default selection with all continents)",
+                id="alert-fade2",
+                dismissable=True,
+                is_open=False,
+                color='warning'
+            )], style={"padding": "10px"}),
+
+        # dynamic page content
+        dbc.Row(html.Div([overview_layout, table_layout, description_layout]))
+
     ], className='container'),
-    # dynamic page content
-    html.Div(id="page-content", style={"margin": "10px"})
+
 ])
+
 
 # =========================
 # Server
 # =========================
+# -------------------------
+# Edge case selection notification / enforcement
+# -------------------------
 @app.callback(Output('index-range-label', 'children'),
               Input('index-dropdown', 'value'))
 def update_label(index_value):
     return f'Select a {index_value} range'
+
 
 @app.callback(
     Output("alert-fade", "is_open"),
@@ -242,6 +273,7 @@ def enforce_range(value):
             return True, [value[0] - 20, value[1]]
     return False, value
 
+
 @app.callback(
     Output("alert-fade2", "is_open"),
     Output('continent-dropdown', 'value'),
@@ -253,25 +285,57 @@ def enforce_continent_select(value):
     else:
         return False, value
 
-# @app.callback(
-#     Output('datatable', 'data'),
-#     Input('continent-dropdown', 'value'),
-#     Input('index-dropdown', 'value'),
-#     Input('range-slider', 'value'))
-# def update_data(continent_value, index_value, range_value):
-#     filtered_data = df.loc[df['Continent'].isin(continent_value)]
-#     filtered_data = filtered_data.loc[filtered_data[index_value].between(range_value[0], range_value[1])]
-#     print(filtered_data.shape)
-#     return filtered_data.to_dict('records')
+# -------------------------
+# Dynamic page layout
+# -------------------------
 
-# @app.callback(
-#     Output("alert-fade", "is_open"),
-#     [Input("alert-toggle-fade", "n_clicks")],
-#     [State("alert-fade", "is_open")],
-# )
-# def toggle_alert(n, is_open):
-#     if n:
-#         return not is_open
-#     return is_open
+
+@app.callback(
+    Output("overview-content", "style"),
+    Output("table-content", "style"),
+    Output("description-content", "style"),
+    Input("url", "pathname")
+)
+def render_page_content(pathname):
+    if pathname == "/":
+        return {"display": "block"}, {"display": "none"}, {"display": "none"}
+    elif pathname == "/table":
+        return {"display": "none"}, {"display": "block"}, {"display": "none"}
+    elif pathname == "/description":
+        return {"display": "none"}, {"display": "none"}, {"display": "block"}
+    else:
+        return {"display": "none"}, {"display": "none"}, {"display": "none"}
+
+# -------------------------
+# Update data table content
+# -------------------------
+
+
+@app.callback(
+    Output('datatable', 'data'),
+    Input('continent-dropdown', 'value'),
+    Input('index-dropdown', 'value'),
+    Input('index-slider', 'value'))
+def update_data(continent_value, index_value, range_value):
+    filtered_data = df.loc[df['Continent'].isin(continent_value)]
+    filtered_data = filtered_data.loc[filtered_data[index_value].between(
+        range_value[0], range_value[1])]
+    return filtered_data.to_dict('records')
+
+
+@app.callback(
+    Output('bar', 'figure'),
+    Input('datatable', 'data')
+)
+def get_filtered_data(data):
+    if len(data) == 0:
+        fig = go.Figure()
+        fig.add_annotation(x=1, y=1, text='No resulting data',
+                           showarrow=False, font=dict(size=20))
+        return fig
+    fig = px.bar(data, x='Continent', y='Cost of Living Index')
+    return fig
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
